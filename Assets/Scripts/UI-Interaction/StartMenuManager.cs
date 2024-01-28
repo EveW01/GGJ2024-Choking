a7bfaec47c079369d184cdf60ab62c42c8247449
EveW01 <1938843274@qq.com>
2024-01-28 11:59:52 +0000

    update

--- Assets/Scripts/UI-Interaction/StartMenuManager.cs
+++ Assets/Scripts/UI-Interaction/StartMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class StartMenuManager : MonoBehaviour
@@ -10,11 +11,37 @@ public class StartMenuManager : MonoBehaviour
 
     public AudioClip buttonClikSound;
 
+    public GameObject mangaBlackBG;
+    public PlayableDirector mangaTimeline; // Timeline 引用
+
     public void StartGame()
     {
-        
-        SceneManager.LoadScene(1);
+        // 播放 Timeline
+        mangaTimeline.Play();
         SFXPlayer.instance.PlaySound(buttonClikSound);
+
+        mangaBlackBG.SetActive(true);
+
+        // 订阅 Timeline 结束事件
+        mangaTimeline.stopped += OnTimelineStopped;
+    }
+
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        // 确保是我们关心的 Timeline
+        if (director == mangaTimeline)
+        {
+            // 加载游戏场景
+            LoadGameScene();
+
+            // 取消订阅事件以避免重复调用
+            mangaTimeline.stopped -= OnTimelineStopped;
+        }
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
     }
 
     public void OpenSettingPanel()
